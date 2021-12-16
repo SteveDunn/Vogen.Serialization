d127302cfff5fa9414d97dcab315b1e508f29806
Steve Dunn <steve.dunn@man.com>
2021-12-16 08:42:16 +0000

    Added newtonsoft converter

--- src/Vogen.Serialization.JsonNet/JsonNetConverterFactory.cs
+++ src/Vogen.Serialization.JsonNet/JsonNetConverterFactory.cs
@@ -1,11 +1,52 @@
 using System;
-using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
 
 namespace Vogen.Serialization.JsonNet;
 
-internal class JsonNetConverterFactory : JsonConverterFactory
-{
-    public override bool CanConvert(Type typeToConvert) => throw new NotImplementedException();
+    /// <summary>
+    /// Converts objects to types that are Vogen value objects, so that they can be deserialized.
+    /// </summary>
+    public class NewtonsoftStringBasedValueObjectConverter : TypeConverter
+    {
+        private readonly MethodInfo? _fromStringMethod;
 
-    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options) => throw new NotImplementedException();
-}
+        /// <summary>
+        /// //todo:
+        /// </summary>
+        /// <param name="destinationType">todo:</param>
+        public NewtonsoftStringBasedValueObjectConverter(Type destinationType)
+        {
+            _fromStringMethod = TryGetBuilderMethod("FromString", destinationType) ??
+                                TryGetBuilderMethod("FromText", destinationType);
+        }
+
+        private static MethodInfo? TryGetBuilderMethod(string methodName, Type destinationType)
+        {
+            return destinationType.GetMethod(
+                methodName,
+                BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Static);
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string) && _fromStringMethod != null)
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object? ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                return _fromStringMethod?.Invoke(null, new[] { value }) ?? throw new InvalidOperationException(
+                    "Value type cannot be converted from a string as there is no public static 'FromString' method defined.");
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
---
+++ tests/Vogen.Tests/JsonNetTests/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+using Vogen.Serialization.JsonNet;
+
+namespace Vogen.SerializationTests.JsonNetTests
+{
+    public class DeserialiseJson
+    {
+        public class NewtonsoftStringBasedValueObjectConverterTests
+    {
+        [Fact]
+        public void Deserialising()
+        {
+            var name = JsonConvert.DeserializeObject<Name>(@"");
+        }
+
+
+
+
+
+
+        private class Person
+        {
+            public Name Name { get; set; } = Name.Uninitialised;
+
+            public Age Age { get; set; } = string.Empty;
+        }
+    }
+
+        [TypeConverter(typeof(NewtonsoftStringBasedValueObjectConverter))]
+        [ValueObject(typeof(string))]
+        [Instance(name: "Invalid", value: "xxx")]
+        public partial class Name
+        {
+            private static Validation Validate(string value)
+            {
+                if (value.Length > 0)
+                    return Validation.Ok;
+
+                return Validation.Invalid("name cannot be empty");
+            }
+        }
+
+        [TypeConverter(typeof(NewtonsoftStringBasedValueObjectConverter))]
+        [ValueObject(typeof(int))]
+        [Instance(name: "Invalid", value: "xxx")]
+        public partial class Age
+        {
+            private static Validation Validate(int value)
+            {
+                if (value >= 0)
+                    return Validation.Ok;
+
+                return Validation.Invalid("age cannot be negative");
+            }
+        }
+    }
