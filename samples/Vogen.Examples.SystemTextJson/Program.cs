9a2887a7d27c11c1a1284a1c1896c61c8a3c9e89
stevedunnhq <steve@dunnhq.com>
2022-01-07 09:06:42 +0000

    Add examples. Update Vogen version.

---
+++ samples/Vogen.Examples.SystemTextJson/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Vogen.Examples.SystemTextJson.SystemTextJsonExamples;
+
+try
+{
+    Console.WriteLine($"{Assembly.GetEntryAssembly()?.FullName} examples:");
+
+    new NonStrict().Run();
+    new Strict().Run();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("OK!");
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Error.WriteLine(ex);
+}
+finally
+{
+    Console.ResetColor();
+}
---
+++ samples/Vogen.Examples.SystemTextJson/SystemTextJsonExamples/Strict.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+using Vogen.Serialization.SystemTextJson;
+
+namespace Vogen.Examples.SystemTextJson.SystemTextJsonExamples;
+
+internal class Strict
+{
+    static readonly JsonSerializerOptions _strictSerializerSettings = new()
+    {
+        Converters =
+        {
+            new VogenConverterFactory(
+                new VogenSerializationOptions
+                {
+                    IsStrict = true
+                })
+        }
+    };
+
+    public void Run()
+    {
+        SerializationOfGoodValues();
+        DeserializeZeroShouldThrow();
+        DeserializeUnspecifiedShouldThrow();
+    }
+
+    private static void SerializationOfGoodValues()
+    {
+        var originalVo = CustomerId.From(123);
+
+        var json = JsonSerializer.Serialize(originalVo, _strictSerializerSettings);
+
+        CustomerId newVo = JsonSerializer.Deserialize<CustomerId>(json, _strictSerializerSettings);
+
+        if (newVo != originalVo)
+        {
+            throw new InvalidOperationException("should be the same before and after serialization!");
+        }
+    }
+
+    private static void DeserializeZeroShouldThrow()
+    {
+        string invalidValue = "0";
+
+        try
+        {
+            JsonSerializer.Deserialize<CustomerId>(invalidValue, _strictSerializerSettings);
+        }
+        catch (ValueObjectValidationException)
+        {
+            return;
+        }
+
+        throw new InvalidOperationException("was expecting an exception!");
+    }
+
+    private static void DeserializeUnspecifiedShouldThrow()
+    {
+        try
+        {
+            var originalVo = CustomerId.Unspecified;
+
+            var json = JsonSerializer.Serialize(originalVo, _strictSerializerSettings);
+            CustomerId vo = JsonSerializer.Deserialize<CustomerId>(json, _strictSerializerSettings);
+        }
+        catch (ValueObjectValidationException)
+        {
+            return;
+        }
+
+        throw new InvalidOperationException("was expecting an exception!");
+    }
+}
